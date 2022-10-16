1a0aef4aed44d6bdb7ca238565e5d7f59d3c2fa5
Michael Fanous <michael@fano.us>
2022-10-15 22:46:27 -0700

    basics

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,27 +1,29 @@
-# Needs uvicorn and fastapi are installs needed
-
-Totals = {}
-frequency = {}
+# Needs uvicorn and fastapi are installs needed pymongo
+class Processing:
+	Totals = {}
+	frequency = {}
 
 #Generate tuple associating each observation with score:
-def cleanup(data):
-	Max = 0
-	Max_food = ""
-	for observation in data:
+	def cleanup(data):
+		Max = 0
+		Max_food = ""
+		for observation in data:
 	#line below assumes I figured out how to tie observation to scores
-    	if score<7:
-			for food in observation:
-				if food in Totals:
-					Totals[food]+=score
-					frequency[food]+=1
-				else:
-					Totals[food]=score
-					frequency[food]=1
-	for elem in Totals:
-		if (Totals[elem]/frequency[elem])> Max:
-			Max_food = elem
-			Max = Totals[elem]/frequency[elem]
-	return(Max_food)
+    		if score<7:
+				for food in observation:
+					if food in Totals:
+						Totals[food]+=score
+						frequency[food]+=1
+					else:
+						Totals[food]=score
+						frequency[food]=1
+		for elem in Totals:
+			if (Totals[elem]/frequency[elem])> Max:
+				Max_food = elem
+				Max = Totals[elem]/frequency[elem]
+		return(Max_food)
 
-cleanup(data=)
+algostart = Processing()
+algostart.cleanup(data=)
 #
+
